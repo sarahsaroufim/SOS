4fe3c2cbdbbdb81ebc9be62d623805a300b23ff8
Sarah Saroufim <143719748+sarahsaroufim@users.noreply.github.com>
2024-04-08 00:45:29 -0400

    m

--- assets/main.js
+++ assets/main.js
@@ -12,20 +12,92 @@ const renderItems = (data) => {
 			`<button class="button-category">${item.emergency_guides}</button>
 
 			<div class="instructions-container">
-			<h2>${item.emergency_guides}</h2>
+			<h3>${item.emergency_guides}</h3>
 			<ol id="instructions">
-				<h3>Instructions</h3>
-				<li class="step">${item.step1}</li><hr class="solid">
-				<li class="step">${item.step2}</li><hr class="solid">
-				<li class="step">${item.step3}</li><hr class="solid">
-				<li class="step">${item.step4}</li><hr class="solid">
-				<li class="step">${item.step5}</li><hr class="solid">
-				<li class="step">${item.step6}</li><hr class="solid">
-				<li class="step">${item.step7}</li><hr class="solid">
-				<li class="step">${item.step8}</li><hr class="solid">
-				<li id="step-9">${item.step9}</li>
+				<h4>Instructions</h4>
+				<li class="step"><a id="step1_details">${item.step1}</a></li><hr class="solid">
+				<li class="step"><a id="step2_details">${item.step2}</a></li><hr class="solid">
+				<li class="step"><a id="step3_details">${item.step3}</a></li><hr class="solid">
+				<li class="step"><a id="step4_details">${item.step4}</a></li><hr class="solid">
+				<li class="step"><a id="step5_details">${item.step5}</a></li><hr class="solid">
+				<li class="step"><a id="step6_details">${item.step6}</a></li><hr class="solid">
+				<li class="step"><a id="step7_details">${item.step7}</a></li><hr class="solid">
+				<li class="step"><a id="step8_details">${item.step8}</a></li><hr class="solid">
+				<li id="step-9"><a id="step9_details">${item.step9}</a></li>
 			</ol>
-		</div>
+			</div>
+
+			<section id="step1_details" class="instruction-details">
+				<h3>${item.emergency_guides}</h3>
+				<h4 class="step-bold">${item.step1}</h4>
+				<p>${item.step1_details}</p>
+				<div class="image-container">${item.step1_image}</div>
+				<p>${item.step1_details2}</p>
+			</section>
+
+			<section id="step2_details" class="instruction-details">
+				<h3>${item.emergency_guides}</h3>
+				<h4 class="step-bold">${item.step2}</h4>
+				<p>${item.step2_details}</p>
+				<div class="image-container">${item.step2_image}</div>
+				<p>${item.step2_details2}</p>
+			</section>
+
+			<section id="step3_details" class="instruction-details">
+				<h3>${item.emergency_guides}</h3>
+				<h4 class="step-bold">${item.step3}</h4>
+				<p>${item.step3_details}</p>
+				<div class="image-container">${item.step3_image}</div>
+				<p>${item.step3_details2}</p>
+			</section>
+
+			<section id="step4_details" class="instruction-details">
+				<h3>${item.emergency_guides}</h3>
+				<h4 class="step-bold">${item.step4}</h4>
+				<p>${item.step4_details}</p>
+				<div class="image-container">${item.step4_image}</div>
+				<p>${item.step4_details2}</p>
+			</section>
+
+			<section id="step5_details" class="instruction-details">
+				<h3>${item.emergency_guides}</h3>
+				<h4 class="step-bold">${item.step5}</h4>
+				<p>${item.step5_details}</p>
+				<div class="image-container">${item.step5_image}</div>
+				<p>${item.step5_details2}</p>
+			</section>
+
+			<section id="step6_details" class="instruction-details">
+				<h3>${item.emergency_guides}</h3>
+				<h4 class="step-bold">${item.step6}</h4>
+				<p>${item.step6_details}</p>
+				<div class="image-container">${item.step6_image}</div>
+				<p>${item.step6_details2}</p>
+			</section>
+
+			<section id="step7_details" class="instruction-details">
+				<h3>${item.emergency_guides}</h3>
+				<h4 class="step-bold">${item.step7}</h4>
+				<p>${item.step7_details}</p>
+				<div class="image-container">${item.step7_image}</div>
+				<p>${item.step7_details2}</p>
+			</section>
+
+			<section id="step8_details" class="instruction-details">
+				<h3>${item.emergency_guides}</h3>
+				<h4 class="step-bold">${item.step8}</h4>
+				<p>${item.step8_details}</p>
+				<div class="image-container">${item.step8_image}</div>
+				<p>${item.step8_details2}</p>
+			</section>
+
+			<section id="step9_details" class="instruction-details">
+			<h3>${item.emergency_guides}</h3>
+			<h4 class="step-bold">${item.step9}</h4>
+			<p>${item.step9_details}</p>
+			<div class="image-container">${item.step9_image}</div>
+			<p>${item.step9_details2}</p>
+		</section>
 			`
 
 		dataList.insertAdjacentHTML('beforeend', listItem) // Add it to the `ul`!
